ce9f74d5540c81a31b97498744d002fcee630777
Nicole Rothenay <nthorn1211@gmail.com>
2019-11-19 16:02:08 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -3,7 +3,7 @@
 class Cat
   
   # uses attr_accessor to read/write aka set/get name
- 
+ # this replaces setting and getting @name and @name=
   attr_accessor :name
  
   # defines a method meow that when called prints "meow!"
