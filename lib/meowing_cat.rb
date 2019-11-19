9d7ab7e3414f8795dc41362026f17c3238254469
Nicole Rothenay <nthorn1211@gmail.com>
2019-11-19 16:01:14 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,6 +1,7 @@
 ## code your solution here. 
 # Defines a class
 class Cat
+  
   # uses attr_accessor to read/write aka set/get name
   attr_accessor :name
   # defines a method meow that when called prints "meow!"
